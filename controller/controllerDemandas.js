bb1357b2730a22a33351d01649ec8eb1446b596f
Michael-Rafael-Andrade <michaelrafaeldeandrade@gmail.com>
2026-05-13 21:49:19 -0300

    Update controllerDemandas.js

--- controller/controllerDemandas.js
+++ controller/controllerDemandas.js
@@ -1,6 +1,6 @@
 const Demanda = require('../model/modelos');
 
-exports.cria_get = function(req, res){
+exports.cria_get = function (req, res) {
     const contexto = {
         titulo_pagina: "Criar nova demanda",
     }
@@ -8,7 +8,7 @@ exports.cria_get = function(req, res){
     res.render('cria_demanda', contexto);
 }
 
-exports.cria_post = async function(req, res){
+exports.cria_post = async function (req, res) {
     const nova_demanda = {
         titulo: req.body.titulo,
         texto: req.body.texto,
@@ -16,11 +16,16 @@ exports.cria_post = async function(req, res){
         // status padrão 'pendente', definido no modelo
     };
 
-    await Demanda.create(nova_demanda);
-    return res.redirect('/');
+    try {
+        await Demanda.create(nova_demanda);
+        return res.redirect('/');
+    } catch (error) {
+        console.error('Erro ao criar demanda: ', error);
+        return res.status(500).send('Erro ao criar demanda');
+    }
 };
 
-exports.consulta = async function(req, res){
+exports.consulta = async function (req, res) {
     const id_demanda = req.params.id;
     const demanda = await Demanda.findByPk(id_demanda);
 
@@ -35,13 +40,13 @@ exports.consulta = async function(req, res){
     res.render('consulta_demanda', contexto);
 }
 
-exports.altera_status = async function(req, res){
+exports.altera_status = async function (req, res) {
     const id_demanda = req.params.id;
     const novo_status = req.params.novo_status;
 
     await Demanda.update(
-        {status: novo_status},  // novos valores dos atributos
-        { where: {id: id_demanda}} // condição para encontrar a demanda a ser utilizada
+        { status: novo_status },  // novos valores dos atributos
+        { where: { id: id_demanda } } // condição para encontrar a demanda a ser utilizada
     );
     return res.redirect('/');
 }
